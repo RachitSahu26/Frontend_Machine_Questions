0f37a24db78e42a03c9fe95518faac09fc16804b
Rachit Sahu <129506267+RachitSahu26@users.noreply.github.com>
2026-01-26 23:19:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import ChipsInput from './Components/InputChips'
 import OTPValidation from './Components/OTPValidation'
-
+import EvenOrOdd from "./Components/EvenOrOdd"; // adjust path if needed
 function App() {
  
 
@@ -15,7 +15,10 @@ function App() {
 <OTPValidation/>
 {/* ..................second project........... */}
 <ChipsInput/>
-
+{/* ..........................third project check number is even or odd............ */}
+   <div>
+        <EvenOrOdd />
+      </div> 
         </div>
     </>
   )
